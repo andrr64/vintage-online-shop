dda46304052cf5a6e1323a863820626d2f6ca02c
andrr64 <derzandrr03@gmail.com>
2025-09-16 20:04:17 +0700

    feat: Add user association and primary address flag to Alamat model - Add UserID field with index and JSON serialization - Add IsPrimary boolean flag with default false and JSON serialization

--- vintage-server/models/alamat.go
+++ vintage-server/models/alamat.go
@@ -6,10 +6,12 @@ import (
 
 type Alamat struct {
 	gorm.Model
+	UserID        uint `gorm:"index" json:"user_id"`
 	AlamatLengkap string
 	Kel           int
 	Kec           int
 	Kab           int
 	Prov          int
 	KodePos       string
+	IsPrimary     bool `gorm:"default:false" json:"is_primary"` // menandai alamat utama
 }
