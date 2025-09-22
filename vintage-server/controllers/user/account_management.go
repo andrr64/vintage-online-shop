97c433a44311c6467a34ffb584e2927204d49391
andrr64 <derzandrr03@gmail.com>
2025-09-22 13:24:12 +0700

    feat: update login endpoint tags and secure cookie settings; implement logout and get account functionality

--- vintage-server/controllers/user/account_management.go
+++ vintage-server/controllers/user/account_management.go
@@ -54,14 +54,14 @@ func Register(c *gin.Context) {
 // Login godoc
 // @Summary User login
 // @Description Login with email and password. Returns JWT token in response and sets httpOnly cookie.
-// @Tags Manajemen-Akun
+// @Tags User/Account Management
 // @Accept json
 // @Produce json
 // @Param input body dto.LoginUserDTO true "Login credentials"
 // @Success 200 {object} dto.CommonResponse[map[string]string]
 // @Failure 400 {object} dto.CommonResponse[string]
 // @Failure 401 {object} dto.CommonResponse[string]
-// @Router /api/v1/user/login [post]	
+// @Router /api/v1/user/login [post]
 func Login(c *gin.Context) {
 	var input dto.LoginUserDTO
 	if err := c.ShouldBindJSON(&input); err != nil {
@@ -90,7 +90,7 @@ func Login(c *gin.Context) {
 		3600*24,        // umur (1 hari, dalam detik)
 		"/",            // path
 		"",             // domain (kosong = current domain)
-		true,           // secure (true kalau pakai HTTPS)
+		false,          // secure (true kalau pakai HTTPS)
 		true,           // httpOnly
 	)
 
@@ -99,5 +99,70 @@ func Login(c *gin.Context) {
 		Message: "Login successful",
 		Success: true,
 		Data:    &map[string]string{"token": token},
+	})
+}
+
+// Logout godoc
+// @Summary Logout user
+// @Description Invalidate user session by clearing JWT cookie
+// @Tags User/Account Management
+// @Accept json
+// @Produce json
+// @Success 200 {object} dto.CommonResponse[string]
+// @Router /api/v1/user/logout [post]
+func Logout(c *gin.Context) {
+	// Hapus cookie JWT
+	c.SetCookie(
+		"access_token",
+		"",
+		-1, // expire segera
+		"/",
+		"",
+		true,
+		true,
+	)
+
+	c.JSON(http.StatusOK, dto.CommonResponse[string]{
+		Message: "User logged out successfully",
+		Success: true,
+		Data:    nil,
+	})
+}
+
+func GetAccount(c *gin.Context) {
+	// Ambil user dari context / JWT middleware
+	currentUserI, exists := c.Get("currentUser")
+	if !exists {
+		c.JSON(http.StatusUnauthorized, dto.CommonResponse[string]{
+			Message: "Unauthorized",
+			Success: false,
+			Data:    nil,
+		})
+		return
+	}
+
+	currentUser := currentUserI.(map[string]interface{})
+
+	userID := currentUser["id"].(uint)
+	user, err := userService.FindByID(userID)
+
+	response := dto.ResponseUserInfoDTO{
+		Username: user.Username,
+		Email:    user.Email,
+		Fullname: user.Fullname,
+	}
+
+	if err != nil {
+		c.JSON(http.StatusNotFound, dto.CommonResponse[any]{
+			Message: "User not found",
+			Success: false,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, dto.CommonResponse[dto.ResponseUserInfoDTO]{
+		Message: "OK",
+		Success: true,
+		Data:    &response,
 	})
 }
