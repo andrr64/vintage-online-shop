437524930dac2b4bf25942f6fe4c87761cad4687
andrr64 <derzandrr03@gmail.com>
2025-09-28 15:55:44 +0700

    refactor: modularize user service and integrate cloudinary - Add auth, middleware, and uploader packages for enhanced functionality - Integrate Cloudinary service for file uploads in user management - Restructure configuration handling to centralize environment variables - Remove redundant config and main files to streamline project structure

--- vintage-server/cmd/user-service/main.go
+++ vintage-server/cmd/user-service/main.go
@@ -8,7 +8,10 @@ import (
 	_ "github.com/lib/pq"
 
 	user "vintage-server/internal/service/account" // Sesuaikan path
+	"vintage-server/pkg/auth"
 	"vintage-server/pkg/config"
+	"vintage-server/pkg/middleware"
+	"vintage-server/pkg/uploader"
 )
 
 func main() {
@@ -16,32 +19,50 @@ func main() {
 	cfg, err := config.LoadConfig()
 	if err != nil {
 		log.Fatalf("could not load config: %v", err)
+		return
 	}
 
 	// 2. Koneksi Database menggunakan config
 	db, err := sqlx.Connect("postgres", cfg.DSN())
 	if err != nil {
 		log.Fatalf("Failed to connect to DB: %v", err)
+		return
 	}
 
 	// 2. Merakit semua lapisan (Wiring)
+	cloudinaryService, err := uploader.NewCloudinaryUploader(cfg.CloudinaryURL)
+	if err != nil {
+		log.Fatalf("Failed to connec to Cloudinary service: %v", err)
+		return
+	}
+
 	userRepo := user.NewRepository(db)
-	userService := user.NewService(userRepo, cfg.JWTSecretKey) // Asumsi service.go sudah dibuat
+	userService := user.NewService(userRepo, cfg.JWTSecretKey, cloudinaryService)
 	userHandler := user.NewHandler(userService)
+	authService := auth.NewJWTService(cfg.JWTSecretKey)
 
 	// 3. Setup Router Gin
 	router := gin.Default()
 
-	// 4. Daftarkan rute ke method di Handler
-	// Ini adalah "API Contract" yang sesungguhnya
+	// 4. Daftar rute ke method di Handler
 	api := router.Group("/api/v1") // Grup rute untuk versioning
 	{
 		account := api.Group("/account")
 		{
+			protected := account.Group("/protected")
+			{
+				protected.Use(middleware.AuthMiddleware(authService))
+				{
+					protected.POST("/logout", userHandler.Logout)
+					protected.PUT("/update-profile", userHandler.UpdateProfile)
+				}
+			}
+
 			customer := account.Group("/customer")
 			{
 				customer.POST("/register", userHandler.RegisterCustomer)
 				customer.POST("/login", userHandler.LoginCustomer)
+
 			}
 			admin := account.Group("/admin")
 			{
