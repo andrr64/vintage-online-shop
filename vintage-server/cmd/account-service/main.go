4361a2c2b26f2eab2c58ddb79020e9063c167844
andrr64 <derzandrr03@gmail.com>
2025-10-01 00:39:37 +0700

    feat: Setup Docker & Air Dev Environments and Product Service Implementation
    - Added Dockerfile and .air.toml for hot reloading in development
    - Renamed user-service to account-service
    - Implemented basic Product Service with category endpoints
    - Updated .gitignore to exclude superuser file

---
+++ vintage-server/cmd/account-service/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+
+	user "vintage-server/internal/service/account" // Sesuaikan path
+	"vintage-server/pkg/auth"
+	"vintage-server/pkg/config"
+	"vintage-server/pkg/middleware"
+	"vintage-server/pkg/uploader"
+)
+
+func main() {
+	// 1. Muat Konfigurasi
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Fatalf("could not load config: %v", err)
+		return
+	}
+
+	// 2. Koneksi Database menggunakan config
+	db, err := sqlx.Connect("postgres", cfg.DSN())
+	if err != nil {
+		log.Fatalf("Failed to connect to DB: %v", err)
+		return
+	}
+
+	// 2. Merakit semua lapisan (Wiring)
+	cloudinaryService, err := uploader.NewCloudinaryUploader(cfg.CloudinaryURL)
+	if err != nil {
+		log.Fatalf("Failed to connec to Cloudinary service: %v", err)
+		return
+	}
+
+	userRepo := user.NewRepository(db)
+	userService := user.NewService(userRepo, cfg.JWTSecretKey, cloudinaryService)
+	userHandler := user.NewHandler(userService)
+	authService := auth.NewJWTService(cfg.JWTSecretKey)
+
+	// 3. Setup Router Gin
+	router := gin.Default()
+
+	// 4. Daftar rute ke method di Handler
+	api := router.Group("/api/v1") // Grup rute untuk versioning
+	{
+		account := api.Group("/account")
+		{
+			protected := account.Group("/protected")
+			{
+				protected.Use(middleware.AuthMiddleware(authService))
+				{
+					protected.POST("/logout", userHandler.Logout)
+					protected.PUT("/update-profile", userHandler.UpdateProfile)
+					protected.PUT("/update-avatar", userHandler.UpdateAvatar)
+
+					protected.POST("/address", userHandler.CreateAddress)
+					protected.PUT("/address", userHandler.UpdateAddress)
+					protected.GET("/address", userHandler.GetAddresses)
+					protected.DELETE("/address", userHandler.DeleteAddress)
+
+					protected.PUT("/address/set-primary", userHandler.SetPrimaryAddress)
+				}
+			}
+
+			customer := account.Group("/customer")
+			{
+				customer.POST("/register", userHandler.RegisterCustomer)
+				customer.POST("/login", userHandler.LoginCustomer)
+
+			}
+			admin := account.Group("/admin")
+			{
+				admin.POST("/login", userHandler.LoginAdmin)
+			}
+		}
+
+	}
+	// 5. Jalankan server
+	log.Printf("User Service running on port : %s", cfg.UserServicePort)
+	router.Run(fmt.Sprint(":", cfg.UserServicePort))
+}
--- vintage-server/cmd/product-service/main.go
+++ vintage-server/cmd/product-service/main.go
@@ -1,10 +1,62 @@
 package main
 
 import (
+	"fmt"
+
+	_ "github.com/lib/pq"
+
 	"log"
+	"vintage-server/internal/service/product"
+	"vintage-server/pkg/auth"
+	"vintage-server/pkg/config"
+	"vintage-server/pkg/middleware"
+	"vintage-server/pkg/uploader"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jmoiron/sqlx"
 )
 
 func main() {
-	log.Printf("Service product sedang berjalan...\n")
-	// TODO: Inisialisasi server, dependency injection, dll.
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Fatalf("could not load config: %v", err)
+		return
+	}
+
+	// 2. Koneksi Database menggunakan config
+	db, err := sqlx.Connect("postgres", cfg.DSN())
+	if err != nil {
+		log.Fatalf("Failed to connect to DB: %v", err)
+		return
+	}
+
+	cloudinaryService, err := uploader.NewCloudinaryUploader(cfg.CloudinaryURL)
+	if err != nil {
+		log.Fatalf("Failed to connect to Cloudinary service: %v", err)
+		return
+	}
+
+	productRepo := product.NewRepository(db)
+	productService := product.NewService(productRepo, cfg.JWTSecretKey, cloudinaryService)
+	productHandler := product.NewHandler(productService)
+	authService := auth.NewJWTService(cfg.JWTSecretKey)
+
+	router := gin.Default()
+
+	api := router.Group("/api/v1")
+	{
+		product := api.Group("/product")
+		{
+			product.GET(("/category"), productHandler.ReadCategories)
+			protected := product.Group("/protected", middleware.AuthMiddleware(authService))
+			{
+				protected.POST(("/category"), productHandler.CreateCategory)
+				protected.PUT(("/category"), productHandler.UpdateCategory)
+				protected.DELETE(("/category"), productHandler.DeleteCategory)
+			}
+
+		}
+	}
+	log.Printf("Product Service running on port :%s", cfg.ProductServicePort)
+	router.Run(fmt.Sprintf(":%s", cfg.ProductServicePort))
 }
